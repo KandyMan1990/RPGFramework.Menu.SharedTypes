2f67a7e923ad71d06e6ecb8952812a6ce4ea9b1d
Elliott Armstrong <earmstrong1990@hotmail.co.uk>
2026-01-18 21:45:32 +0000

    Add additional types so modules outside of MenuModule can access menu types

--- Runtime/SharedTypes.cs
+++ Runtime/SharedTypes.cs
@@ -4,16 +4,40 @@ using RPGFramework.Core.SharedTypes;
 
 namespace RPGFramework.Menu.SharedTypes
 {
+    public enum MenuType
+    {
+        Inventory,
+        Abilities,
+        CharacterInfo,
+        Config,
+        Save
+    }
+    
+    public interface IMenuTypeProvider
+    {
+        Type GetType(MenuType type);
+    }
+    
     public interface IMenuModuleArgs : IModuleArgs
     {
         Type MenuType { get; }
     }
-    
-    public readonly struct MenuModuleArgs<TMenu> : IMenuModuleArgs
+
+    public readonly struct GenericMenuModuleArgs<TMenu> : IMenuModuleArgs
     {
         public Type MenuType => typeof(TMenu);
     }
-    
+
+    public readonly struct MenuModuleArgs : IMenuModuleArgs
+    {
+        public Type MenuType { get; }
+
+        public MenuModuleArgs(Type menuType)
+        {
+            MenuType = menuType;
+        }
+    }
+
     public interface IMenuModule : IModule
     {
         Task PushMenu(IMenuModuleArgs menuModuleArgs);
